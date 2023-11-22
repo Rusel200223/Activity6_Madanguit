6961f1ba1df0a0c81364f08257b0977a9bffc9ce
Rusel200223 <ruselninomandanguit@gmail.com>
2023-11-22 19:36:26 +0800

    Activity#6

---
+++ user.js
@@ -0,0 +1,66 @@
+
+
+export const users = [
+
+    {
+        id: 1,
+        name: {
+            firstname: "Rusel",
+            lastname: "Madanguit",
+        },
+        course: "BSIT",
+        yearsection: "3A",
+        username: "xchaser",
+        password: "qwerty23",
+        usertype: "Student",
+    },
+    {
+        id: 2,
+        name: {
+            firstname: "Arnel",
+            lastname: "chicboi",
+        },
+        course: "BSIT",
+        yearsection: "3A",
+        username: "Maotganon",
+        password: "arnel123",
+        usertype: "Student",
+    },
+    {
+        id: 3,
+        name: {
+            firstname: "Rica",
+            lastname: "Mwuah2",
+        },
+        course: "BSFPSM",
+        yearsection: "3A",
+        username: "Hakkkkdog",
+        password: "asdfg",
+        usertype: "Student",
+    },
+    {
+        id: 4,
+        name: {
+            firstname: "Jerry",
+            lastname: "Ale",
+        },
+        course: "",
+        yearsection: "",
+        username: "Mazter",
+        password: "zirrr",
+        usertype: "Teacher",
+    },
+    {
+        id: 5,
+        name: {
+            firstname: "FJ",
+            lastname: "Asufra",
+        },
+        course: "",
+        yearsection: "",
+        username: "Bords",
+        password: "hideout",
+        usertype: "Teacher",
+    },
+
+];
